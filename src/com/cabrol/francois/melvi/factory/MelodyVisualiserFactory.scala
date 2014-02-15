71816bb93cbbe6a6e9ee508098ce95f47361b5a7
francoiscabrol <francois.cabrol@live.fr>
2014-02-15 18:00:31 -0500

    Initial commit

--- src/com/cabrol/francois/melvi/factory/MelodyVisualiserFactory.scala
+++ src/com/cabrol/francois/melvi/factory/MelodyVisualiserFactory.scala
@@ -1,20 +1,20 @@
 /*
  * Copyright (c) 2014 François Cabrol.
  *
- *  This file is part of MelodyVisualiser.
+ *  This file is part of MelVi.
  *
- *     MelodyVisualiser is free software: you can redistribute it and/or modify
+ *     MelVi is free software: you can redistribute it and/or modify
  *     it under the terms of the GNU General Public License as published by
  *     the Free Software Foundation, either version 3 of the License, or
  *     (at your option) any later version.
  *
- *     MelodyVisualiser is distributed in the hope that it will be useful,
+ *     MelVi is distributed in the hope that it will be useful,
  *     but WITHOUT ANY WARRANTY; without even the implied warranty of
  *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *     GNU General Public License for more details.
  *
  *     You should have received a copy of the GNU General Public License
- *     along with MelodyVisualiser.  If not, see <http://www.gnu.org/licenses/>.
+ *     along with MelVi.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package com.cabrol.francois.melvi.factory

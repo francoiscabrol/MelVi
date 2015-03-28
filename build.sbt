8e263a47069450d7f4a0c02eccd6c4f5d9a1a762
francoiscabrol <francois.cabrol@live.fr>
2015-03-28 13:29:12 -0400

    Create Build.scala

--- build.sbt
+++ build.sbt
@@ -1,25 +1,2 @@
 // Project name
 name := "melvi"
-
-// Project version
-version := "0.3.0"
-
-// Scala version
-scalaVersion := "2.11.4"
-
-javacOptions ++= Seq("-source", "1.8", "-target", "1.8", "-Xlint")
-
-initialize := {
-  val _ = initialize.value
-  if (sys.props("java.specification.version") != "1.8")
-    sys.error("Java 8 is required for this project.")
-}
-
-// Fix bug with MidiSystem under MacOs
-connectInput in run := true
-fork in run := true
-
-unmanagedJars in Compile += Attributed.blank(file(System.getenv("JAVA_HOME") + "/jre/lib/ext/jfxrt.jar"))
-
-// Add dependency on ScalaFX library from Maven repository
-libraryDependencies += "org.scalafx" %% "scalafx" % "8.0.20-R6"
---
+++ project/Build.scala
@@ -0,0 +1,52 @@
+import sbt.Keys._
+import sbt._
+
+object MelviBuild extends Build {
+
+  //////////////////////////////////////////////////////////////////////////////
+  // PROJECT INFO
+  //////////////////////////////////////////////////////////////////////////////
+
+  val ORGANIZATION = "francoiscabrol"
+  val PROJECT_NAME = "melvi"
+  val PROJECT_VERSION = "0.3.0"
+  val SCALA_VERSION = "2.11.4"
+
+  //////////////////////////////////////////////////////////////////////////////
+  // PROJECTS
+  //////////////////////////////////////////////////////////////////////////////
+
+  lazy val root = Project(
+    id = PROJECT_NAME,
+    base = file("."),
+    settings = melviSettings
+  )
+
+  //////////////////////////////////////////////////////////////////////////////
+  // SETTINGS
+  //////////////////////////////////////////////////////////////////////////////
+
+  lazy val melviSettings = Project.defaultSettings ++ basicSettings
+
+  lazy val basicSettings = Seq(
+    version := PROJECT_VERSION,
+    organization := ORGANIZATION,
+    scalaVersion := SCALA_VERSION,
+
+    javacOptions ++= Seq("-source", "1.8", "-target", "1.8", "-Xlint"),
+
+    initialize := {
+      val _ = initialize.value
+      if (sys.props("java.specification.version") != "1.8")
+        sys.error("Java 8 is required for this project.")
+    },
+
+    libraryDependencies += ("org.scalafx" %% "scalafx" % "8.0.20-R6"),
+
+    unmanagedJars in Compile += Attributed.blank(file(System.getenv("JAVA_HOME") + "/jre/lib/ext/jfxrt.jar")),
+
+    connectInput in run := true,
+
+    fork in run := true
+  )
+}
